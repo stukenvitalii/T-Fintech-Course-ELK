8df5f534280b51cd1400d475dd5b1a004c8ae6e4
stukenvitalii <stukenvitalii@gmail.com>
2024-11-23 12:50:12 +0300

    init commit

---
+++ src/main/java/org/tinkoff/tfintechcourseelk/TFintechCourseElkApplication.java
@@ -0,0 +1,13 @@
+package org.tinkoff.tfintechcourseelk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TFintechCourseElkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TFintechCourseElkApplication.class, args);
+    }
+
+}
